523679ad33955e5f71f9f568712a520cee97b2b7
Sumit Mondal <117383564+sumit0012@users.noreply.github.com>
2024-04-27 12:56:39 +0530

    Update cancer_app.py

--- app/cancer_app.py
+++ app/cancer_app.py
@@ -2,20 +2,12 @@ import gradio as gr
 from skops.io import load
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
-import csv
-
-# Ensure data_cancer.csv exists
-file_path = "data_cancer.csv"
-with open(file_path, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    # Write header if needed
-    writer.writerow(['diagnosis', 'texture_mean', 'symmetry_mean', 'texture_se', 'area_se', 'smoothness_se', 'concavity_se', 'symmetry_se', 'fractal_dimension_se', 'smoothness_worst'])
 
 # Load the pre-trained model
 pipe = load("Model/cancer_pipeline.skops", trusted=True)
 
-def predict_cancer(texture_mean, symmetry_mean, texture_se, area_se, smoothness_se, concavity_se, symmetry_se, fractal_dimension_se, smoothness_worst):
-    features = [[texture_mean, symmetry_mean, texture_se, area_se, smoothness_se, concavity_se, symmetry_se, fractal_dimension_se, smoothness_worst]]
+def predict_cancer(texture_mean,symmetry_mean,texture_se,area_se,smoothness_se,concavity_se,symmetry_se,fractal_dimension_se,smoothness_worst):
+    features = [[texture_mean,symmetry_mean,texture_se,area_se,smoothness_se,concavity_se,symmetry_se,fractal_dimension_se,smoothness_worst]]
     # Standardize features using the same scaler used during training
     # scaler = StandardScaler()
     # features_scaled = scaler.fit_transform(features)
@@ -25,13 +17,14 @@ def predict_cancer(texture_mean, symmetry_mean, texture_se, area_se, smoothness_
     # print(predicted_cancer)
     features[0] = [predicted_cancer] + features[0]
     
-    temp_df = pd.read_csv(file_path)
+    temp_df = pd.read_csv("Data/data_cancer.csv")
     new_df = pd.DataFrame(features, columns=['diagnosis', 'texture_mean', 'symmetry_mean', 'texture_se', 'area_se', 'smoothness_se', 'concavity_se', 'symmetry_se', 'fractal_dimension_se', 'smoothness_worst'])
     temp_df = pd.concat([temp_df, new_df], ignore_index=True)
     temp_df = temp_df.drop_duplicates()
-    temp_df.to_csv(file_path, header=True, index=False)
+    temp_df.to_csv("Data/data_cancer.csv", header=True, index=False)
 
     return label
+    # if predicted_cancer == 1 :
 
 inputs = [
     gr.Slider(1, 40, step=0.1, label="texture_mean"),
@@ -48,9 +41,9 @@ inputs = [
 outputs = gr.Label(num_top_classes=2)
 
 examples = [
-    [10.38, 0.2419, 0.9053, 153.40, 0.006399, 0.05373, 0.03003, 0.006193, 0.16220],
-    [24.54, 0.1587, 1.4280, 19.15, 0.007189, 0.00000, 0.02676, 0.002783, 0.08996],
-    [17.77, 0.1812, 0.7339, 74.08, 0.005225, 0.01860, 0.01389, 0.003532, 0.12380]
+    [10.38,0.2419,0.9053,153.40,0.006399,0.05373,0.03003,0.006193,0.16220],
+    [24.54,0.1587,1.4280,19.15,0.007189,0.00000,0.02676,0.002783,0.08996],
+    [17.77,0.1812,0.7339,74.08,0.005225,0.01860,0.01389,0.003532,0.12380]
 ]
 
 title = "Breast Cancer Prediction"
@@ -66,4 +59,4 @@ gr.Interface(
     description=description,
     article=article,
     theme=gr.themes.Soft(),
-).launch(debug=False)
+).launch(debug = True)
